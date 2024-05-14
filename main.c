7f5cca3105e2e83c62dfd38a17c1ff18afeeb0e2
n30nx <n30nxsec@protonmail.com>
2024-05-14 04:46:34 +0300

    chore: added comments and removed the contracts for a little while, gonna need to add them at some point

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <limits.h>
 #include <time.h>
 
+// This part is for allowing me to work from my MacBook
 #if defined(__APPLE__)
     #include <malloc/malloc.h>
     #define malloc_usable_size(x) malloc_size(x)
@@ -20,6 +21,7 @@
 #if defined(__x86_64__)
     #include <immintrin.h>
 #endif
+//
 
 #if defined(DEBUG)
     #define DBG(fmt, ...) do {                                  \
@@ -31,10 +33,17 @@
     #define DBG(...)
 #endif
 
+// Didn't want to link maths library for just max and min functions
 #define max(a, b) a > b ? a : b
 #define min(a, b) a < b ? a : b
 
-typedef int16_t *matrix_t;
+/*
+ * I've used uint16_t because:
+ * 1- If I need to do 1000x1000 * 1000x1000 matrix multiplication I'm
+ * gonna need way more bits and it's not feasible
+ * 2- I'd like to think the elements of the matrix in \every M_{i} \in Z/2^16Z
+ */
+typedef uint16_t *matrix_t;
 
 #pragma region mem
 
@@ -63,19 +72,6 @@ static inline matrix_t matrix_new(uint16_t row, uint16_t col) {
     return new;
 }
 
-/*
- * Free Matrix Memory
- *
- * Bu işlev, dinamik olarak tahsis edilmiş belleği serbest bırakır ve bir matrisin bellek sızıntısını önler.
- *
- * @param matrix: Serbest bırakılacak matris
- * @param row: Matrisin satır sayısı
- *
- * @pre matrix NULL olmamalıdır.
- * @pre row sıfırdan farklı olmalıdır.
- *
- * @post Matrisin belleği serbest bırakılmalıdır.
- */
 __attribute__((always_inline))
 static inline void matrix_free(matrix_t matrix) {
 #if defined(__x86_64__)
@@ -87,134 +83,6 @@ static inline void matrix_free(matrix_t matrix) {
 #pragma endregion // mem
 
 #pragma region utils
-
-/*
- * Assign Random Values to Matrix
- *
- * Bu işlev, belirtilen satır ve sütun sayısına sahip bir matrise rastgele tam sayı değerleri atar.
- *
- * @param matrix: Değerlerin atanacağı matris
- * @param row: Matrisin satır sayısı
- * @param col: Matrisin sütun sayısı
- *
- * @pre matrix NULL olmamalıdır.
- * @pre row ve col sıfırdan farklı olmalıdır.
- *
- * @post Matris, her bir elemanı 0 ile INT16_MAX (hariç) arasında rastgele bir tam sayı ile doldurulmalıdır.
- */
-__attribute__((always_inline))
-static inline void matrix_assign_random(matrix_t matrix, uint16_t row, uint16_t col) {
-    for (uint16_t i = 0; i < row; i++) {
-        for (uint16_t j = 0; j < col; j++) {
-            matrix[i * col + j] = rand() % INT16_MAX;
-        }
-    }
-}
-
-/*
- * Print Matrix
- *
- * This function prints the elements of the matrix to the standard output.
- *
- * @param matrix: Matrix to be printed
- * @param row: Number of rows in the matrix
- * @param col: Number of columns in the matrix
- *
- * @pre matrix must not be NULL.
- * @pre row and col should be greater than 0.
- *
- * @post The elements of the matrix are printed to the standard output.
- */
-__attribute__((always_inline))
-static inline void matrix_print(FILE *stream, matrix_t __restrict matrix, uint16_t row, uint16_t col) {
-    for (uint16_t i = 0; i < row; i++) {
-        for (uint16_t j = 0; j < col; j++) {
-            fprintf(stream, "%hu ", matrix[i * col + j]);
-        }
-        fprintf(stream, "\n");
-    }
-    fprintf(stream, "\n\n\n");
-}
-
-/*
- * Matrix Addition
- *
- * This function adds matrix a and matrix b element-wise and stores the result in matrix c.
- *
- * @param c: Resultant matrix
- * @param a: Matrix operand A
- * @param b: Matrix operand B
- * @param size: Size of the matrices (number of rows or columns)
- *
- * @pre c, a, and b must not be NULL.
- * @pre size should be greater than 0.
- *
- * @post Matrix c contains the result of the addition of matrix a and matrix b.
- */
-__attribute__((always_inline))
-static inline void matrix_add(matrix_t c, matrix_t a, matrix_t __restrict b, uint16_t size) {
-    uint32_t num_elements = size * size;
-#if defined(__x86_64__)
-    for (uint32_t i = 0; i < num_elements; i += 16) {
-        if (i + 16 <= num_elements) {  // ensure we do not go out of bounds
-            __m256i vec_a = _mm256_load_si256((__m256i*)(a + i));  // load 8 elements from a
-            __m256i vec_b = _mm256_load_si256((__m256i*)(b + i));  // load 8 elements from b
-            __m256i vec_sum = _mm256_add_epi16(vec_a, vec_b);     // add the elements
-            _mm256_store_si256((__m256i*)(c + i), vec_sum);        // store the result in c
-        } else {
-            // handle the case where remaining elements are less than 8
-            for (uint32_t j = i; j < num_elements; j++) {
-                c[j] = a[j] + b[j]; // scalar addition for remaining elements
-            }
-            break; // exit the loop after handling remaining elements
-        }
-    }
-#else
-    for (uint32_t i = 0; i < num_elems; i++) {
-        c[i] = a[i] + b[i];
-    }
-#endif
-}
-
-/*
- * Matrix Subtraction
- *
- * This function subtracts matrix b from matrix a element-wise and stores the result in matrix c.
- *
- * @param c: Resultant matrix
- * @param a: Matrix operand A
- * @param b: Matrix operand B
- * @param size: Size of the matrices (number of rows or columns)
- *
- * @pre c, a, and b must not be NULL.
- * @pre size should be greater than 0.
- *
- * @post Matrix c contains the result of the subtraction of matrix b from matrix a.
- */
-__attribute__((always_inline))
-static inline void matrix_sub(matrix_t c, matrix_t a, matrix_t __restrict b, uint16_t size) {
-    uint32_t num_elements = size * size;
-#if defined(__x86_64__)
-    for (uint32_t i = 0; i < num_elements; i += 16) {
-        if (i + 16 <= num_elements) {  // ensure we do not go out of bounds
-            __m256i vec_a = _mm256_load_si256((__m256i*)(a + i));  // load 8 elements from a
-            __m256i vec_b = _mm256_load_si256((__m256i*)(b + i));  // load 8 elements from b
-            __m256i vec_sum = _mm256_sub_epi16(vec_a, vec_b);     // add the elements
-            _mm256_store_si256((__m256i*)(c + i), vec_sum);        // store the result in c
-        } else {
-            // handle the case where remaining elements are less than 8
-            for (uint32_t j = i; j < num_elements; j++) {
-                c[j] = a[j] - b[j]; // scalar addition for remaining elements
-            }
-            break; // exit the loop after handling remaining elements
-        }
-    }
-#else
-    for (uint32_t i = 0; i < num_elems; i++) {
-        c[i] = a[i] - b[i];
-    }
-#endif
-}
 
 static const int log_table[64] = {
     63,  0, 58,  1, 59, 47, 53,  2,
@@ -231,6 +99,7 @@ static const int log_table[64] = {
  * de Brujin sequence
  * http://supertech.csail.mit.edu/papers/debruijn.pdf
  */
+// Fast log2 algorithm
 __attribute__((always_inline))
 static inline int u16_log2(size_t size) {
     size |= size >> 1;
@@ -254,32 +123,83 @@ static inline uint16_t round_size(uint16_t size) {
     return size;
 }
 
-/*
- * Pad Matrix
- *
- * This function performs padding to resize a matrix to a specified dimension.
- *
- * @param dest: Destination address of the padded matrix
- * @param src: Address of the original matrix to be padded
- * @param dest_row: Number of rows in the padded matrix
- * @param dest_col: Number of columns in the padded matrix
- * @param src_row: Number of rows in the original matrix
- * @param src_col: Number of columns in the original matrix
- *
- * @pre dest and src must not be NULL.
- * @pre dest_row and dest_col must be non-zero.
- * @pre src_row and src_col must be non-zero.
- * @pre The difference between dest_row and src_row should be less than or equal to the difference between src_col and dest_col.
- * @pre src_col value must be greater than or equal to dest_col value.
- *
- * @post The padded matrix should be filled with the data from the original matrix.
- * @post The empty part of the padded matrix should be filled with zeros.
- */
+__attribute__((always_inline))
+static inline void matrix_assign_random(matrix_t matrix, uint16_t row, uint16_t col) {
+    // Pretty straight-forward, assigns random (mod 2^16) to all indexes
+    for (uint16_t i = 0; i < row; i++) {
+        for (uint16_t j = 0; j < col; j++) {
+            matrix[i * col + j] = rand() % UINT16_MAX;
+        }
+    }
+}
+
+__attribute__((always_inline))
+static inline void matrix_print(FILE *stream, matrix_t __restrict matrix, uint16_t row, uint16_t col) {
+    // Writes values to the given stream
+    for (uint16_t i = 0; i < row; i++) {
+        for (uint16_t j = 0; j < col; j++) {
+            fprintf(stream, "%hu ", matrix[i * col + j]);
+        }
+        fprintf(stream, "\n");
+    }
+    fprintf(stream, "\n\n\n");
+}
+
+__attribute__((always_inline))
+static inline void matrix_add(matrix_t c, matrix_t a, matrix_t __restrict b, uint16_t size) {
+    uint32_t num_elements = size * size;
+#if defined(__x86_64__)
+    for (uint32_t i = 0; i < num_elements; i += 16) {
+        if (i + 16 <= num_elements) {  // So we do not go out of bounds
+            __m256i vec_a = _mm256_load_si256((__m256i*)(a + i));  // Load 16 elements from a
+            __m256i vec_b = _mm256_load_si256((__m256i*)(b + i));  // Load 16 elements from b
+            __m256i vec_sum = _mm256_add_epi16(vec_a, vec_b);      // Add the elements
+            _mm256_store_si256((__m256i*)(c + i), vec_sum);        // Store the result in c
+        } else {
+            // Handle the case where remaining elements are less than 16
+            for (uint32_t j = i; j < num_elements; j++) {
+                c[j] = a[j] + b[j]; // Non-intrinsic addition for remaining elements
+            }
+            return; // If we've got this far, there are no more elements left
+        }
+    }
+#else
+    for (uint32_t i = 0; i < num_elems; i++) {
+        c[i] = a[i] + b[i]; // Normal matrix addition
+    }
+#endif
+}
+
+__attribute__((always_inline))
+static inline void matrix_sub(matrix_t c, matrix_t a, matrix_t __restrict b, uint16_t size) {
+    uint32_t num_elements = size * size;
+#if defined(__x86_64__)
+    for (uint32_t i = 0; i < num_elements; i += 16) {
+        if (i + 16 <= num_elements) {  // So we do not go out of bounds
+            __m256i vec_a = _mm256_load_si256((__m256i*)(a + i));  // Load 16 elements from a
+            __m256i vec_b = _mm256_load_si256((__m256i*)(b + i));  // Load 16 elements from b
+            __m256i vec_sum = _mm256_sub_epi16(vec_a, vec_b);      // Subtract the elements
+            _mm256_store_si256((__m256i*)(c + i), vec_sum);        // Store the result in c
+        } else {
+            // Handle the case where remaining elements are less than 16
+            for (uint32_t j = i; j < num_elements; j++) {
+                c[j] = a[j] - b[j]; // Non-intrinsic subtraction for remaining elements
+            }
+            return; // If we've got this far, there are no more elements left
+        }
+    }
+#else
+    for (uint32_t i = 0; i < num_elems; i++) {
+        c[i] = a[i] - b[i]; // Normal matrix subraction
+    }
+#endif
+}
+
 void matrix_pad(matrix_t dest, matrix_t __restrict src, uint16_t dest_row, uint16_t dest_col, uint16_t src_row, uint16_t src_col) {
 #if defined(__x86_64__)
     __m256i zero = _mm256_setzero_si256();
     for (uint16_t i = 0; i < dest_row; i++) {
-        for (uint16_t j = 0; j < dest_col; j += 16) {  // Assuming int16_t, 16 values per 256-bit register
+        for (uint16_t j = 0; j < dest_col; j += 16) {  // Using uint16_t, 16 values per 256-bit register
             _mm256_store_si256((__m256i*)(dest + i * dest_col + j), zero);
         }
     }
@@ -287,44 +207,22 @@ void matrix_pad(matrix_t dest, matrix_t __restrict src, uint16_t dest_row, uint1
     // Now copy src to dest, taking care of possible misalignment
     for (uint16_t i = 0; i < src_row; i++) {
         uint16_t j = 0;
-        for (; j < src_col - 15; j += 16) {  // copy in blocks of 16, assuming src_col is large enough
+        for (; j < src_col - 15; j += 16) {  // Copy in blocks of 16, assuming src_col is large enough
             __m256i data = _mm256_loadu_si256((__m256i*)(src + i * src_col + j));
             _mm256_storeu_si256((__m256i*)(dest + i * dest_col + j), data);
         }
-        for (; j < src_col; j++) {  // handle remaining elements if any
+        for (; j < src_col; j++) {  // Handle remaining elements if any
             dest[i * dest_col + j] = src[i * src_col + j];
         }
     }
 #else
-    memset(dest, 0, dest_row * dest_col * sizeof(uint16_t));
+    memset(dest, 0, dest_row * dest_col * sizeof(uint16_t)); // Write zeroes
     for (uint16_t i = 0; i < src_row; i++) {
         memcpy(dest + dest_col * i, src + src_col * i, src_col * sizeof(uint16_t));  // Copy original data
-        // memset(dest + (dest_col * i) + src_col, 0, (dest_col - src_col) * sizeof(int16_t));  // Pad remaining columns with zeros
     }
-    // Pad remaining rows with zeros
-    /*for (int16_t i = src_row; i < dest_row; i++) {
-        memset(dest + (dest_col * i), 0, dest_col * sizeof(int16_t));
-    }*/
 #endif
 }
 
-/*
- * Unpad Matrix
- *
- * This function performs unpadding to resize a matrix to a specified dimension.
- *
- * @param dest: Destination address of the unpadded matrix
- * @param src: Address of the original matrix to be unpadded
- * @param dest_row: Number of rows in the unpadded matrix
- * @param dest_col: Number of columns in the unpadded matrix
- * @param src_size: Size of the original square matrix
- *
- * @pre dest and src must not be NULL.
- * @pre dest_row and dest_col must be non-zero.
- * @pre src_size must be non-zero.
- *
- * @post The unpadded matrix should be filled with the data from the original matrix.
- */
 __attribute__((always_inline))
 static inline void matrix_unpad(matrix_t dest, matrix_t __restrict src, uint16_t dest_row, uint16_t dest_col, uint16_t src_size) {
     DBG("dest_start = %p, dest_end = %p, length = %hu", dest, dest + dest_row * dest_col, dest_row * dest_col);
@@ -338,10 +236,13 @@ static inline void matrix_unpad(matrix_t dest, matrix_t __restrict src, uint16_t
 
 __attribute__((always_inline, unused))
 static inline void matrix_eq(matrix_t __restrict a, matrix_t __restrict b, uint16_t m, uint16_t l) {
-    for (uint32_t i = 0; i < m * l; i++) {
-        if (a[i] != b[i]) {
-            printf("Function is wrong!\n");
-            return;
+    // Check if matrixes are equal (used for validating multiplication)
+    for (uint16_t i = 0; i < m; i++) {
+        for (uint16_t j = 0; j < l; j++) {
+            if (a[i * l + j] != b[i * l + j]) {
+                printf("Function is wrong!\n");
+                return;
+            }
         }
     }
     printf("Function is correct!\n");
@@ -349,7 +250,7 @@ static inline void matrix_eq(matrix_t __restrict a, matrix_t __restrict b, uint1
 #pragma endregion // utils
 
 // FrodoKEM Matrix Multiplication
-// TODO: https://eprint.iacr.org/2021/711.pdf
+// REFS: https://eprint.iacr.org/2021/711.pdf
 //       https://github.com/microsoft/PQCrypto-LWEKE/blob/a2f9dec8917ccc3464b3378d46b140fa7353320d/FrodoKEM/src/frodo_macrify.c#L252
 //
 void matrix_multiply(matrix_t c, matrix_t a, matrix_t b, uint16_t m, uint16_t n, uint16_t l) {
@@ -409,29 +310,15 @@ void matrix_multiply(matrix_t c, matrix_t a, matrix_t b, uint16_t m, uint16_t n,
 }
 
 // Strassen Algorithm
-// TODO: https://en.wikipedia.org/wiki/Strassen_algorithm
-/*
- * Strassen Matrix Multiplication
- *
- * This function implements the Strassen algorithm for matrix multiplication.
- *
- * @param c: Destination matrix for the result of multiplication
- * @param a: First matrix to be multiplied
- * @param b: Second matrix to be multiplied
- * @param size: Size of the matrices (assumed to be a power of 2)
- *
- * @pre c, a, and b must not be NULL.
- * @pre size must be a power of 2.
- *
- * @post The destination matrix c will contain the result of multiplying matrices a and b using the Strassen algorithm.
- */
+// REF: https://en.wikipedia.org/wiki/Strassen_algorithm
 static void strassen(matrix_t c, matrix_t __restrict a, matrix_t __restrict b, uint16_t size) {
+    // I know this looks shady but it's the best one for cache-misses, doesn't matter small or big numbers, 512 does the trick.
     if (size <= 512) {
         matrix_multiply(c, a, b, size, size, size);
     } else {
         uint16_t new_size = size / 2;
 
-        // bootstrapping submatrices
+        // Bootstrap submatrices
         matrix_t a11 = matrix_new(new_size, new_size);
         matrix_t a12 = matrix_new(new_size, new_size);
         matrix_t a21 = matrix_new(new_size, new_size);
@@ -458,7 +345,7 @@ static void strassen(matrix_t c, matrix_t __restrict a, matrix_t __restrict b, u
         matrix_t tmp_a = matrix_new(new_size, new_size);
         matrix_t tmp_b = matrix_new(new_size, new_size);
 
-        // dividing to submatrices
+        // Divide to submatrices
         for (uint16_t i = 0; i < new_size; i++) {
             for (uint16_t j = 0; j < new_size; j++) {
                 a11[i * new_size + j] = a[i * size + j];
@@ -532,6 +419,7 @@ static void strassen(matrix_t c, matrix_t __restrict a, matrix_t __restrict b, u
         matrix_sub(c22, c22, p3, new_size);
         matrix_sub(c22, c22, p7, new_size);
         
+        // Place the output to the relevant quadrants
         for (uint16_t i = 0; i < new_size; i++) {
             for (uint16_t j = 0; j < new_size; j++) {
                 c[i * size + j] = c11[i * new_size + j]; 
@@ -541,6 +429,7 @@ static void strassen(matrix_t c, matrix_t __restrict a, matrix_t __restrict b, u
             }
         }
 
+        // Free used memory
         matrix_free(a11);
         matrix_free(a12);
         matrix_free(a21);
@@ -569,23 +458,6 @@ static void strassen(matrix_t c, matrix_t __restrict a, matrix_t __restrict b, u
     }
 }
 
-/*
- * Matrix Preparation and Multiplication using Strassen Algorithm
- *
- * This function prepares the input matrices by padding them to a suitable size for the Strassen algorithm
- * and then performs matrix multiplication using the Strassen algorithm.
- *
- * @param c: Destination matrix for the result of multiplication
- * @param a: First matrix to be multiplied
- * @param b: Second matrix to be multiplied
- * @param m: Number of rows in matrix a
- * @param n: Number of columns in matrix a (and number of rows in matrix b)
- * @param l: Number of columns in matrix b
- *
- * @pre c, a, and b must not be NULL.
- *
- * @post The destination matrix c will contain the result of multiplying matrices a and b.
- */
 void matrix_prepare_and_mul(matrix_t c, matrix_t __restrict a, matrix_t __restrict b, uint16_t m, uint16_t n, uint16_t l) {
     uint16_t new_size = round_size(max(max(m, n), l));
     new_size = new_size < 512 ? 512 : new_size;
@@ -613,14 +485,17 @@ void matrix_prepare_and_mul(matrix_t c, matrix_t __restrict a, matrix_t __restri
 int main(int argc, char **argv) {
     uint16_t m, n, l;
 
+    // Read the params from stdin if supplied
     if (argc == 4) {
         sscanf(*(argv + 1), "%hu", &m);
         sscanf(*(argv + 2), "%hu", &n);
         sscanf(*(argv + 3), "%hu", &l);
     } else {
+        // Read the params
         scanf("%hu %hu %hu", &m, &n, &l);
     }
 
+    // Create matrixes
     matrix_t a_matrix = matrix_new(m, n);
     matrix_t b_matrix = matrix_new(n, l);
     matrix_t c_matrix = matrix_new(m, l);
@@ -643,13 +518,16 @@ int main(int argc, char **argv) {
 #else
     matrix_assign_random(a_matrix, m, n);
     matrix_assign_random(b_matrix, n, l);
-#endif    
+#endif
+
     matrix_prepare_and_mul(c_matrix, a_matrix, b_matrix, m, n, l);
-    
+
+    // Write the matrix to a file in order to keep the stdout clean.
     FILE *matrix_out = fopen("matrix_output_main.txt", "w");
     matrix_print(matrix_out, c_matrix, m, l);
     fclose(matrix_out);
 
+    // Free used memory
     matrix_free(a_matrix);
     matrix_free(b_matrix);
     matrix_free(c_matrix);
